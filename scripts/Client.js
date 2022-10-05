60dbaafddf20997d29c30b91bc200dc0bde762a3
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-05 18:34:17 +0200

    Exercise concluded.

--- scripts/Client.js
+++ scripts/Client.js
@@ -1,16 +1,17 @@
 class Client {
-    constructor () {
-        //Your token goes here
-        this.token = "";
-    }
+  constructor() {
+    //Your token goes here
+    this.token = "b34a0fc5";
+  }
 
-    async getMovieData(movie) {
-        let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`);
-        let data = await response.json();
-        return data;
-    }
+  async getMovieData(movie) {
+    let response = await fetch(
+      `http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`
+    );
+    let data = await response.json();
+    console.log(data);
+    return data;
+  }
 }
-
-console.log("Hello from Client.js");
 
 export default Client;
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,53 @@
-import View from './View.js';
-import Client from './Client.js';
+import View from "./View.js";
+import Client from "./Client.js";
 
-// All of your javascript should go here
-console.log("Hello from index.js");
+const input = document.querySelector("#input");
+const btnSave = document.querySelector(".btn-save");
+const btnReset = document.querySelector(".btn-reset");
+
+const client = new Client();
+const view = new View();
+
+let moviesArr = [];
+
+//
+
+input.addEventListener("change", async function () {
+  if (moviesArr.includes(`${input.value}`))
+    return alert(`${input.value} is  already in your list.`);
+
+  let response = await client.getMovieData(input.value);
+  let data = view.displayMovieOnPage(response);
+
+  moviesArr.push(response);
+
+  // clear input
+  input.value = "";
+});
+
+//
+
+btnSave.addEventListener("click", function () {
+  localStorage.setItem("movies", JSON.stringify(moviesArr));
+});
+
+loadMovies();
+function loadMovies() {
+  const localItems = localStorage.getItem("movies");
+  let parsed = JSON.parse(localItems);
+
+  if (localItems) {
+    moviesArr.push(...parsed);
+
+    moviesArr.forEach((el) => {
+      view.displayMovieOnPage(el);
+    });
+  }
+}
+//
+
+btnReset.addEventListener("click", () => {
+  view.removeDisplay();
+  localStorage.removeItem("movies");
+  moviesArr.splice(0);
+});
